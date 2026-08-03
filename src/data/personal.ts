fe2ee469290071653747a641bc5a49f34795e086
MoisesTH <62824374+MoisesTH@users.noreply.github.com>
2026-08-02 21:52:39 -0600

    Actualizar personal.ts

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -9,10 +9,10 @@ export const personalData: PersonalInfo = {
     "Investigador en Ciencias del Comportamiento",
     "Especialista en Análisis de Datos & Inferencia Bayesiana"
   ],
-  email: "[CORREO ELECTRÓNICO]", // Sustituir por tu correo real, ej: moises.torres@ejemplo.com
-  githubUrl: "[URL DE GITHUB]", // Sustituir por tu GitHub real, ej: https://github.com/moisestorres
-  linkedinUrl: "[URL DE LINKEDIN]", // Sustituir por tu LinkedIn real, ej: https://www.linkedin.com/in/moisestorres
-  location: "Ciudad de México, México",
+  email: "[moisesth55555@gmail.com]", // Sustituir por tu correo real, ej: moises.torres@ejemplo.com
+  githubUrl: "[https://github.com/MoisesTH]", // Sustituir por tu GitHub real, ej: https://github.com/moisestorres
+  linkedinUrl: "[https://www.linkedin.com/in/mois%C3%A9s-torres-hern%C3%A1ndez-922571238/]", // Sustituir por tu LinkedIn real, ej: https://www.linkedin.com/in/moisestorres
+  location: "Ixtapaluca, Estado de México",
   bioSummary: "Licenciado en Psicología por la UNAM con perfil híbrido en tecnología e investigación. Especializado en programación en Python, enseñanza técnica, metodologías ágiles y modelamiento bayesiano jerárquico aplicado a la toma de decisiones.",
   detailedBio: [
     "Soy licenciado en Psicología por la Universidad Nacional Autónoma de México (UNAM), con experiencia en enseñanza, capacitación tecnológica, programación con Python, gestión de proyectos y metodologías ágiles.",
